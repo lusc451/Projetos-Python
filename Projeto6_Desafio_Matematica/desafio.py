d3729222b250002ce6ce70021491b59dd19fa527
Lucas Antunes <cleiton.lucas416@gmail.com>
2024-09-24 07:54:06 -0300

    Problema 6 - Desafio de Matemática

--- Projeto6_Desafio_Matematica/desafio.py
+++ Projeto6_Desafio_Matematica/desafio.py
@@ -5,6 +5,7 @@ NESSE CÓDIGO, VAMOS ELABORAR UM DESAFIO NO QUAL SERÃO EXIBIDOS PROBLEMAS MATEM
 SOLUCIONAR, NÃO SERÁ EXIBIDO UM NOVO PROBLEMA MATEMÁTICO ATÉ O USUÁRIO SOLUCIONAR O PROBLEMA EXIBIDO. 
 '''
 import random
+import time
 
 OPERADORES = ['+', '-', '*']
 MIN_OPERAND = 3
@@ -21,10 +22,23 @@ def gerar_problema():
     resposta = eval(expressao)
     return expressao, resposta
 
+resp_erradas = 0
+
+input("Aperte enter para começar!")
+print("--------------------------")
+
+tempo_comeco = time.time()
+
 for i in range(TOTAL_PROBLEMAS):
     expressao, resposta = gerar_problema()
     while True:
         tentativa = input("Problema nº" + str(i + 1) + ": " + expressao + " = ")
         if tentativa == str(resposta):
             break
+        resp_erradas += 1
         
+tempo_fim = time.time()
+tempo_total = tempo_fim - tempo_comeco
+
+print("--------------------------")
+print("Mandou bem! Você terminou em ", tempo_total, " segundos!")
