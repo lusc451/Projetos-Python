06fb2e899442fc15f2cc1bea1d6fee17d3c16ce1
Lucas Antunes <cleiton.lucas416@gmail.com>
2024-09-24 07:47:39 -0300

    Projeto 6 - Desafio de matemática

--- Projeto6_Desafio_Matematica/desafio.py
+++ Projeto6_Desafio_Matematica/desafio.py
@@ -4,9 +4,27 @@ PROJETO Nº 6 - DESAFIO DE MATEMÁTICA.
 NESSE CÓDIGO, VAMOS ELABORAR UM DESAFIO NO QUAL SERÃO EXIBIDOS PROBLEMAS MATEMÁTICOS, NO QUAL O USUÁRIO DEVERÁ
 SOLUCIONAR, NÃO SERÁ EXIBIDO UM NOVO PROBLEMA MATEMÁTICO ATÉ O USUÁRIO SOLUCIONAR O PROBLEMA EXIBIDO. 
 '''
-
 import random
 
-OPERADORES = ['+', '-', '/', '*']
+OPERADORES = ['+', '-', '*']
 MIN_OPERAND = 3
 MAX_OPERAND = 12
+TOTAL_PROBLEMAS = 5
+
+def gerar_problema():
+    esquerda = random.randint(MIN_OPERAND, MAX_OPERAND)
+    direita = random.randint(MIN_OPERAND, MAX_OPERAND)
+    
+    operador = random.choice(OPERADORES)
+    
+    expressao = str(esquerda) + " " + operador + " " + str(direita)
+    resposta = eval(expressao)
+    return expressao, resposta
+
+for i in range(TOTAL_PROBLEMAS):
+    expressao, resposta = gerar_problema()
+    while True:
+        tentativa = input("Problema nº" + str(i + 1) + ": " + expressao + " = ")
+        if tentativa == str(resposta):
+            break
+        
